fdb39364f7ecafe4fd8fbc399391d9f6aa7f1b17
fdonizeti <fabiodsilva@yahoo.com.br>
2022-04-08 23:56:29 +0000

    teste atlantis 16

--- commit.sh
+++ commit.sh
@@ -13,7 +13,7 @@ echo "--------------------------------------------------------------"
 git commit -m "teste atlantis $2"
 echo "--------------------------------------------------------------"
 
-git push -u origin $1
-#git push --set-upstream origin $1
+#git push -u origin $1
+git push --set-upstream origin $1
 
 echo "FIM DO COMMIT"
